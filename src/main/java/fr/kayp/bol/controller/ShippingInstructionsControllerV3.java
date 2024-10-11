c117bab48b620a367e7a0e3999c8cfa594032999
Atroooo <lucas.compiegne1@gmail.com>
2024-10-11 17:56:52 +0200

    working, just need to add script to deploy contract

--- src/main/java/fr/kayp/bol/controller/ShippingInstructionsControllerV3.java
+++ src/main/java/fr/kayp/bol/controller/ShippingInstructionsControllerV3.java
@@ -2,86 +2,104 @@ package fr.kayp.bol.controller;
 
 import com.baeldung.openapi.api.V3ApiDelegate;
 import com.baeldung.openapi.model.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.*;
 
+import fr.kayp.bol.repository.ProofOfAuthenticityRepository;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.security.MessageDigest;
 
 @RestController
 @RequestMapping("/v3")
 public class ShippingInstructionsControllerV3 implements V3ApiDelegate { 
 
-    /**
-     * This method is used to create a new shipping instructions document reference.
-     * 
-     * @param apiVersion
-     * @param createShippingInstructions
-     * @return ResponseEntity<Status>
-     */
-    @PostMapping(value = "/shipping-instructions", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<ShippingInstructionsResponse> postShippingInstructionsDocumentReference(
-            @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
-            @RequestBody CreateShippingInstructions createShippingInstructions) {
-    
-            //202 Accepted
-            //400 Bad Request
-            //500 Server Error
-            return ResponseEntity.accepted().body(new ShippingInstructionsResponse());
-    }
+        final ProofOfAuthenticityRepository proofOfAuthenticityRepository;
 
-    @PutMapping(value = "/shipping-instructions/{documentReference}")
-    public ResponseEntity<String> putShippingInstructionsDocumentReference(
-            @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
-            @RequestParam (value = "documentReference", required = true) String documentReference,
-            @RequestParam(value = "updateShippingInstructions", required = true) UpdateShippingInstructions updateShippingInstructions) {
-            
-            //202 Accepted
-            //400 Bad Request
-            //404 Not Found
-            //409 Conflict
-            //500 Server Error
-            return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+        public ShippingInstructionsControllerV3(final ProofOfAuthenticityRepository proofOfAuthenticityRepository) {
+                this.proofOfAuthenticityRepository = proofOfAuthenticityRepository;
+        }
 
-    @GetMapping(value = "/shipping-instructions/{documentReference}")
-    public ResponseEntity<String> getShippingInstructionsDocumentReference(
-            @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
-            @RequestParam (value = "documentReference", required = true) String documentReference,
-            @RequestParam(value = "updatedContent", required = true) String updateShippingInstructions) {
+        /**
+         * This method is used to create a new shipping instructions document reference.
+         * 
+         * @param apiVersion
+         * @param createShippingInstructions
+         * @return ResponseEntity<Status>
+         */
+        @PostMapping(value = "/shipping-instructions", consumes = "application/json", produces = "application/json")
+        public ResponseEntity<ShippingInstructionsResponse> postShippingInstructionsDocumentReference(
+                @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
+                @RequestBody CreateShippingInstructions createShippingInstructions) throws Exception {
+        
+                String jsonEbl = new ObjectMapper().writeValueAsString(createShippingInstructions);
+                
+                MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+                messageDigest.update(jsonEbl.getBytes());
+                String stringHash = new String(messageDigest.digest());
 
-            //200 OK
-            //202 Accepted
-            //404 Not Found
-            //409 Conflict
-            //500 Server Error
-            return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+                String nftId = proofOfAuthenticityRepository.createProofOfAuthenticity(stringHash);
+                //202 Accepted
+                //400 Bad Request
+                //500 Server Error
+                ShippingInstructionsResponse shippingInstructionsResponse = new ShippingInstructionsResponse();
+                return ResponseEntity.accepted().body(shippingInstructionsResponse);
+        }
 
-    @PatchMapping(value = "/shipping-instructions/{documentReference}")
-    public ResponseEntity<String> patchShippingInstructionsDocumentReference(
-            @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
-            @RequestParam (value = "documentReference", required = true) String documentReference,
-            @RequestBody UpdateShippingInstructions updateShippingInstructions) {
+        @PutMapping(value = "/shipping-instructions/{documentReference}")
+        public ResponseEntity<String> putShippingInstructionsDocumentReference(
+                @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
+                @RequestParam (value = "documentReference", required = true) String documentReference,
+                @RequestParam(value = "updateShippingInstructions", required = true) UpdateShippingInstructions updateShippingInstructions) {
+                
+                //202 Accepted
+                //400 Bad Request
+                //404 Not Found
+                //409 Conflict
+                //500 Server Error
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
 
-            //202 Accepted
-            //400 Bad Request
-            //404 Not Found
-            //409 Conflict
-            //500 Server Error
-            return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+        @GetMapping(value = "/shipping-instructions/{documentReference}")
+        public ResponseEntity<String> getShippingInstructionsDocumentReference(
+                @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
+                @RequestParam (value = "documentReference", required = true) String documentReference,
+                @RequestParam(value = "updatedContent", required = true) String updateShippingInstructions) {
 
-    @PostMapping(value = "shipping-instructions-notifications", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<String> postShippingInstructionsNotifications(
-            @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
-            @RequestBody ShippingInstructionsNotification shippingInstructionsNotification) {
+                //200 OK
+                //202 Accepted
+                //404 Not Found
+                //409 Conflict
+                //500 Server Error
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
 
-            //204 No Content
-            //400 Bad Request
-            //500 Server Error
-            return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+        @PatchMapping(value = "/shipping-instructions/{documentReference}")
+        public ResponseEntity<String> patchShippingInstructionsDocumentReference(
+                @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
+                @RequestParam (value = "documentReference", required = true) String documentReference,
+                @RequestBody UpdateShippingInstructions updateShippingInstructions) {
 
+                //202 Accepted
+                //400 Bad Request
+                //404 Not Found
+                //409 Conflict
+                //500 Server Error
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+
+        @PostMapping(value = "shipping-instructions-notifications", consumes = "application/json", produces = "application/json")
+        public ResponseEntity<String> postShippingInstructionsNotifications(
+                @RequestHeader(value = "Api-Version-Major", required = false) String apiVersion,
+                @RequestBody ShippingInstructionsNotification shippingInstructionsNotification) {
+
+                //204 No Content
+                //400 Bad Request
+                //500 Server Error
+                return new ResponseEntity<>(HttpStatus.CREATED);
+        }
 }
